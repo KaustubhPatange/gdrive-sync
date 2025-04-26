0fc77feafb0c6380051d8a12eb21283ac284d269
KaustubhPatange <developerkp16@gmail.com>
2025-04-26 21:22:20 +0530

    feat: add Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+# Setup cron job
+echo "0 2 * * * cd /app && node /app/index.js >> /var/log/cron.log 2>&1" > /etc/crontabs/root
+
+# Start crond in background
+crond -b -l 8
+
+# Output logs
+tail -f /var/log/cron.log
